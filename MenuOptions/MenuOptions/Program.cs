eddccae3281da1bee1b453a86f4934705e4a237e
Lucas Munoz <lucas00792@gmail.com>
2021-09-08 19:20:33 -0500

    Initial Commit

---
+++ MenuOptions/MenuOptions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MenuOptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
